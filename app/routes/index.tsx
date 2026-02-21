7cb54e2f00d5b7b86e6e365b1d016ad9043efe54
Francis Anyuru <anyuruf@gmail.com>
2026-02-21 04:47:42 -0500

    add biome linter/formater

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,16 @@
-import type { Route } from "./+types/index";
 import LandingPage from "~/components/landing";
+import type { Route } from "./+types/index";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Outlet iCom" },
-    { name: "ecommerce platform", content: "Welcome to Outlets ecommerce platform!" },
-  ];
+	return [
+		{ title: "Outlet iCom" },
+		{
+			name: "ecommerce platform",
+			content: "Welcome to Outlets ecommerce platform!",
+		},
+	];
 }
 
 export default function HomePage() {
-  return <LandingPage />;
+	return <LandingPage />;
 }
